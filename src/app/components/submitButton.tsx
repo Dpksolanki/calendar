3d0f900fdd86f8291ab30690dbf02fcc1ec249e7
Deepak Solanki <deepak.solanki2335@gmail.com>
2024-11-26 14:29:03 +0530

    Add the onboarding route

--- src/app/components/submitButton.tsx
+++ src/app/components/submitButton.tsx
@@ -5,7 +5,21 @@ import { useFormStatus } from "react-dom";
 import Logo from "../../../public/github.svg";
 import Image from "next/image";
 import { Loader2 } from "lucide-react";
-
+import { cn } from "@/lib/utils";
+ 
+interface iAppProps {
+    text: string;
+    variant?: 
+    "default" 
+    | "destructive" 
+    | "outline" 
+    | "secondary" 
+    | "ghost" 
+    | "link" 
+    | null 
+    | undefined;
+    className?: string;
+}
 
 export function GithubAuthButton() {
     const { pending } = useFormStatus()
@@ -23,6 +37,21 @@ export function GithubAuthButton() {
                         Sign in with Github
                     </Button>
                 )}
+        </>
+    )
+}
+
+export function SubmitButton({ text , variant, className }: iAppProps) {
+    const {pending} = useFormStatus()
+
+    return(
+        <>
+        {pending ? (<Button disabled variant='outline' className={cn("w-fit",className)}>
+            <Loader2 className="size-4 mr-2 animate-spin"/> please wait
+        </Button>):
+        (
+        <Button type="submit" variant={variant} className={cn("w-fit",className)}>{text}</Button>
+        )}
         </>
     )
 }
